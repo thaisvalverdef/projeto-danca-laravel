290e4364bd182941e7a50fa35cfde072e03770e7
thaisvalverdef <thaisvalverdef@gmail.com>
2020-07-15 18:54:53 -0300

    Correção no tamanho do carousel

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
 
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-      {{-- <ol class="carousel-indicators">
+    <div class="container-fluid" id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+     {{--  <ol class="carousel-indicators">
         <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
         <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
         <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
