7d38a8dd3323c82002fc664680d08467da39d5e6
thaisvalverdef <thaisvalverdef@gmail.com>
2020-08-10 13:33:33 -0300

    Dropdown corrigido

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,7 +49,7 @@
                         <a class="nav-link" href="/como-funciona">Como funciona</a>
                     </li>
 
-                  <li class="nav-item dropdown">
+                <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Modalidades</a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                             <a class="dropdown-item" href="/modalidades">Ballet</a>
